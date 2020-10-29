72f11e40dff673b206af7f0c5db22ddb24ab45d5
kirill.fedyanin <kirill.fedyanin@gmail.com>
2020-10-29 16:49:58 +0300

    Structured dropout

--- experiments/check_cnn.py
+++ experiments/check_cnn.py
@@ -1,93 +1,42 @@
 #%%
-import os
-os.environ["CUDA_VISIBLE_DEVICES"] = '1'
-#%%
+import pandas as pd
+import numpy as np
 import torch
 from torch import nn
 import torch.nn.functional as F
+import seaborn as sns
 from torchvision import datasets as datasets, transforms
 from torch.utils.data import DataLoader
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_curve, roc_auc_score
+import matplotlib.pyplot as plt
+
+from model_resnet import ResNet18, ResNet50, ResNet50_dropblock, ResNet50_dropchannel
+from datasets import get_data
 
 
 #%%
 class Args:
     batch_size = 256
-    train_batch_size = 1000
-    lr = 1e-3
-    gamma = 0.9
+    train_batch_size = 5000
+    lr = 1e-2
+    gamma = 0.95
     log_interval = 100
-    epochs = 100
+    epochs = 2
 
 args = Args()
 
 
 #%%
-transform=transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.1307,), (0.3081,))
-])
-trainset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-trainset, _ = train_test_split(trainset, test_size=2_000)
-testset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
-
-train_loader = DataLoader(trainset, batch_size=args.batch_size, shuffle=True)
-test_loader = DataLoader(testset, batch_size=args.train_batch_size)
-
-#%%
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
-        self.pooling = nn.MaxPool2d(2)
-        self.conv3 = nn.Conv2d(64, 64, 3, padding=1)
-        self.conv4 = nn.Conv2d(64, 64, 3, padding=1)
-        self.pooling = nn.MaxPool2d(4)
-        self.dropout1 = nn.Dropout(0.25)
-        self.fc1 = nn.Linear(64*49, 128)
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(128, 10)
-
-    def forward(self, x):
-        x = F.elu(self.conv1(x))
-        x = F.elu(self.conv2(x))
-        x = self.pooling(x)
-        x = F.elu(self.conv4(x))
-        x = F.elu(self.conv4(x))
-
-        x = torch.flatten(x, 1)
-        x = self.dropout1(x)
-        x = F.elu(self.fc1(x))
-        x = self.dropout2(x)
-        x = self.fc2(x)
-        output = F.log_softmax(x, dim=1)
-        return output
-
-
-def main():
-    model = Net()
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = model.to(device)
-    optimizer = torch.optim.Adadelta(model.parameters(), lr=args.lr)
-
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=args.gamma)
-    for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
-        test(model, device, test_loader)
-        scheduler.step()
-
-
+loaders = get_data('svhn')
 #%%
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
-        import ipdb; ipdb.set_trace()
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
-        loss = F.nll_loss(output, target)
+        loss = F.cross_entropy(output, target)
         loss.backward()
         optimizer.step()
         if batch_idx % args.log_interval == 0:
@@ -104,7 +53,7 @@ def test(model, device, test_loader):
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            test_loss += F.cross_entropy(output, target, reduction='sum').item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
 
@@ -115,5 +64,126 @@ def test(model, device, test_loader):
         100. * correct / len(test_loader.dataset)))
 
 
-if __name__ == '__main__':
-    main()
+#%%
+train_loader = loaders['train']
+test_loader = loaders['valid']
+# model = Net()
+model = ResNet50_dropchannel()
+# model = ResNet50(input_size=3, dropout_rate=0.2)
+
+
+#%%
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
+
+#%%
+optimizer = torch.optim.Adadelta(model.parameters(), lr=args.lr)
+
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=args.gamma)
+for epoch in range(1, args.epochs + 1):
+    train(args, model, device, train_loader, optimizer, epoch)
+    test(model, device, test_loader)
+    scheduler.step()
+#%%
+predictions = []
+labels = []
+probs = []
+with torch.no_grad():
+    for x_batch, y_batch in  test_loader:
+        logits = model(x_batch.cuda())
+        probs.append(torch.max(torch.softmax(logits, dim=-1), dim=-1).values.cpu().numpy())
+        predictions.append(torch.argmax(logits, dim=-1).cpu().numpy())
+        labels.append(y_batch.numpy())
+labels = np.concatenate(labels)
+predictions = np.concatenate(predictions, axis=-1)
+probs = np.concatenate(probs, axis=-1)
+
+#%%
+errors = labels != predictions
+
+#%%
+uq = 1 - probs
+
+#%%
+T = 200
+
+# model.train()
+model.eval()
+predictions = []
+for t in range(T):
+    if (t+1) % 10  == 0:
+        print(t)
+    probs = []
+    for x_batch, y_batch in  test_loader:
+        with torch.no_grad():
+            logits = model(x_batch.cuda(), dropout_mask=True, debug=True)
+        probs.append(torch.softmax(logits, dim=-1).cpu().numpy())
+    predictions.append(np.concatenate(probs, axis=0))
+
+#%%
+predictions = np.array(predictions)
+predictions.shape
+
+#%%
+def entropy(preds):
+    return -np.mean((np.sum(np.log(preds) * preds, axis=-1)), axis=0)
+
+def std_uq(preds):
+    return np.std(np.max(preds, axis=-1), axis=0)
+
+def bald(preds):
+    means = np.mean(preds, axis=0)
+    return - np.sum(np.log(means) * means, axis=-1) + entropy(preds)
+
+def max_with_std(preds):
+    stds = np.std(preds, axis=0)
+    means = np.mean(preds, axis=0)
+    mean_std = means - stds
+    return 1 - np.max(mean_std, axis=-1)
+
+
+acquisition = entropy
+uq_mc = acquisition(predictions)
+#%%
+fpr, tpr, _ = roc_curve(errors, uq)
+plt.plot(fpr, tpr)
+
+fpr, tpr, _ = roc_curve(errors, uq_mc)
+plt.plot(fpr, tpr)
+plt.title("Resnet, Dropout 0.1/0.05, (max prob with std)")
+#%%
+aucs = []
+nums = np.linspace(2, 500, 50)
+max_score = 0
+for num in nums:
+    uq_sub = acquisition(predictions[:int(num)])
+    score = roc_auc_score(errors, uq_sub)
+    if score > max_score:
+        i = num
+        max_score = score
+    aucs.append(score)
+plt.plot(nums, aucs)
+print(i)
+#%%
+
+stds = np.std(predictions, axis=0)
+means = np.mean(predictions, axis=0)
+
+#%%
+df = pd.DataFrame({'errors':errors, 'stds': stds[:, 0]})
+plt.figure(figsize=(10, 8))
+sns.displot(df, x='stds', bins=20)
+plt.title("Mc dropout std distribution")
+
+#%%
+
+#%%
+uq_1 = entropy(predictions)
+uq_2 = max_with_std(predictions)
+plt.scatter(uq_1, uq_2)
+#%%
+mean_std = means - stds
+args_1 = np.argmax(means, axis=-1)
+args_2 = np.argmax(mean_std, axis=-1)
+np.sum(args_1 != args_2)
+#%%
--- experiments/model_resnet.py
+++ experiments/model_resnet.py
@@ -4,6 +4,7 @@ Reference:
 [1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
     Deep Residual Learning for Image Recognition. arXiv:1512.03385
 '''
+from random import random
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -148,6 +149,13 @@ def ResNet50(input_size=3, dropout_rate=0.15):
 def ResNet50_dropblock(input_size=3, dropout_rate=0.15, block_size=3):
     return ResNetDropblock(Bottleneck, [3, 4, 6, 3], input_size=input_size, dropout_rate=dropout_rate)
 
+def ResNet50_dropchannel(input_size=3, dropout_rate=0.15, block_size=3):
+    return ResNetDropchannel(Bottleneck, [3, 4, 6, 3], input_size=input_size, dropout_rate=dropout_rate)
+
+
+def ResNet50_droplayer(input_size=3, dropout_rate=0.15, block_size=3):
+    return ResNetDroplayer([3, 4, 6, 3], input_size=input_size, dropout_rate=dropout_rate)
+
 
 def ResNet101():
     return ResNet(Bottleneck, [3, 4, 23, 3])
@@ -194,23 +202,206 @@ class ResNetDropblock(nn.Module):
             self.in_planes = planes * block.expansion
         return nn.Sequential(*layers)
 
-    def forward(self, x, dropout_rate=0.2, dropout_mask=None):
+    def _cached_drop(self, x, drop_layer, dropout_mask):
+        if dropout_mask is None:
+            return drop_layer(x)
+        else:
+            state = drop_layer.training
+            drop_layer.training = True
+            out = drop_layer(x)
+            drop_layer.training = state
+            return out
+
+    def forward(self, x, dropout_rate=0.2, dropout_mask=None, debug=False):
         # out = F.elu(self.bn1(self.conv1(x)))
         out = self.maxpool(F.elu(self.bn1(self.conv1(x))))
         out = self.layer1(out)
-        out = self.drop_1(out)
+        if debug:
+            import ipdb; ipdb.set_trace()
+        out = self._cached_drop(out, self.drop_1, dropout_mask)
 
         out = self.layer2(out)
-        out = self.drop_2(out)
+        out = self._cached_drop(out, self.drop_2, dropout_mask)
 
         out = self.layer3(out)
-        out = self.drop_3(out)
+        out = self._cached_drop(out, self.drop_3, dropout_mask)
 
         out = self.layer4(out)
-        out = self.drop_4(out)
+        out = self._cached_drop(out, self.drop_4, dropout_mask)
         out = out.view(out.size(0), -1)
 
         out = self.linear(out)
         return out
 
 # test()
+
+class ResNetDropchannel(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10, dropout_rate=0.2, input_size=3, block_size=3):
+        super().__init__()
+        self.in_planes = 64
+
+        self.dropout_rate = dropout_rate
+
+        self.conv1 = nn.Conv2d(input_size, 64, kernel_size=7,
+                               stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.drop_1 = nn.Dropout2d(p=dropout_rate)
+
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.drop_2 = nn.Dropout2d(p=dropout_rate)
+
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.drop_3 = nn.Dropout2d(p=dropout_rate)
+
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.drop_4 = nn.Dropout2d(p=dropout_rate)
+
+        self.dropout = nn.Dropout(dropout_rate)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def _cached_drop(self, x, drop_layer, dropout_mask):
+        if dropout_mask is None:
+            return drop_layer(x)
+        else:
+            state = drop_layer.training
+            drop_layer.training = True
+            out = drop_layer(x)
+            drop_layer.training = state
+            return out
+
+    def forward(self, x, dropout_rate=0.2, dropout_mask=None, debug=False):
+        # out = F.elu(self.bn1(self.conv1(x)))
+        out = self.maxpool(F.elu(self.bn1(self.conv1(x))))
+        out = self.layer1(out)
+        if debug:
+            import ipdb; ipdb.set_trace()
+        out = self._cached_drop(out, self.drop_1, dropout_mask)
+
+        out = self.layer2(out)
+        out = self._cached_drop(out, self.drop_2, dropout_mask)
+
+        out = self.layer3(out)
+        out = self._cached_drop(out, self.drop_3, dropout_mask)
+
+        out = self.layer4(out)
+        out = self._cached_drop(out, self.drop_4, dropout_mask)
+        out = out.view(out.size(0), -1)
+
+        out = self.linear(out)
+        return out
+
+
+
+class DropLayerBottleneck(nn.Module):
+    expansion = 4
+    def __init__(self, in_planes, planes, stride=1, dropout_rate=0.2):
+        super().__init__()
+
+        self.dropout_rate = dropout_rate
+
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion *
+                               planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        shortcut = self.shortcut(x)
+        if random() > self.dropout_rate:
+            out = F.elu(self.bn1(self.conv1(x)))
+            out = F.elu(self.bn2(self.conv2(out)))
+            out = self.bn3(self.conv3(out))
+        else:
+            out = torch.zeros_like(shortcut).to(shortcut.device)
+        out += shortcut
+        out = F.elu(out)
+        return out
+
+
+
+class ResNetDroplayer(nn.Module):
+    def __init__(self, num_blocks, num_classes=10, dropout_rate=0.2, input_size=3, block_size=3):
+        super().__init__()
+        self.in_planes = 64
+        block = DropLayerBottleneck
+
+        self.dropout_rate = dropout_rate
+
+        self.conv1 = nn.Conv2d(input_size, 64, kernel_size=7,
+                               stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.drop_1 = nn.Dropout2d(p=dropout_rate)
+
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.drop_2 = nn.Dropout2d(p=dropout_rate)
+
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.drop_3 = nn.Dropout2d(p=dropout_rate)
+
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.drop_4 = nn.Dropout2d(p=dropout_rate)
+
+        self.dropout = nn.Dropout(dropout_rate)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def _cached_drop(self, x, drop_layer, dropout_mask):
+        if dropout_mask is None:
+            return drop_layer(x)
+        else:
+            state = drop_layer.training
+            drop_layer.training = True
+            out = drop_layer(x)
+            drop_layer.training = state
+            return out
+
+    def forward(self, x, dropout_rate=0.2, dropout_mask=None, debug=False):
+        # out = F.elu(self.bn1(self.conv1(x)))
+        out = self.maxpool(F.elu(self.bn1(self.conv1(x))))
+        out = self.layer1(out)
+        if debug:
+            import ipdb; ipdb.set_trace()
+        out = self._cached_drop(out, self.drop_1, dropout_mask)
+
+        out = self.layer2(out)
+        out = self._cached_drop(out, self.drop_2, dropout_mask)
+
+        out = self.layer3(out)
+        out = self._cached_drop(out, self.drop_3, dropout_mask)
+
+        out = self.layer4(out)
+        out = self._cached_drop(out, self.drop_4, dropout_mask)
+        out = out.view(out.size(0), -1)
+
+        out = self.linear(out)
+        return out
